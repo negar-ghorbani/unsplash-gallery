7a643489a869e2b4651d941ace7926760e49ab13
luega <martinelliluca90@gmail.com>
2023-02-21 14:34:05 +0100

    DROPDOWN MENU
    created dropdown

--- src/fetcher.ts
+++ src/fetcher.ts
@@ -1,71 +1,74 @@
-export const searchButton = document.getElementById('search') as HTMLButtonElement;
+export const searchButton = document.getElementById(
+  'search'
+) as HTMLButtonElement;
 
 localStorage.setItem('myList', '[]');
 
 searchButton.addEventListener('click', () => {
   const search = document.getElementById('input') as HTMLElement;
   const url = `https://api.unsplash.com/search/photos/?client_id=q4swtOlzLBR4BJxfQEgQA-u3_3hNzATacG7eJtjYLcU&query=${search.value}`;
-  
+
   const myList = JSON.parse(localStorage.getItem('myList')!);
+  if (search.value === '' || myList.includes(search.value)) {
+    fetchAPI(url);
+    return myList;
+  }
   myList.push(search.value);
   localStorage.setItem('myList', JSON.stringify(myList));
-  
+
   fetchAPI(url);
 });
+
 export const fetchAPI = async (url: string) => {
   const images = await fetch(url)
-    .then(response => response.json())
-    .then(data => data.results);
+    .then((response) => response.json())
+    .then((data) => data.results);
 
   const container: HTMLElement = document.getElementById('image-container')!;
   container.innerHTML = '';
-  images.forEach(image => {
-    container.innerHTML += `<div>
-        <img src="${image.urls.regular}" alt="${image.alt_description}">
-      </div>`;
+  images.forEach((image) => {
+    container.innerHTML += `
+    <div class="imageDiv">
+      <div class="flip-card">
+      <div class="flip-card-inner">
+    <div class="flip-card-front">
+     <img src="${image.urls.small}" alt="${image.alt_description}" style="width:300px;height:300px;">
+    </div>
+    <div class="flip-card-back">
+     ${image.alt_description}
+    </div>
+  </div>
+</div>
+</div>`;
   });
 };
 
-function autocomplete(input, list) {
-  //Add an event listener to compare the input value with all countries
-  input.addEventListener('input', function () {
-    console.log('hi');
-    
-      //Close the existing list if it is open
-      closeList();
-
-      //If the input is empty, exit the function
-      if (!input.value)
-          return;
-
-      //Create a suggestions <div> and add it to the element containing the input field
-      const suggestions = document.createElement('div');
-      suggestions.setAttribute('id', 'suggestions');
-      input.parentNode.appendChild(suggestions);
-
-      //Iterate through all entries in the list and find matches
-      for (let i=0; i<list.length; i++) {
-          if (list[i].toUpperCase().includes(input.value.toUpperCase())) {
-              //If a match is found, create a suggestion <div> and add it to the suggestions <div>
-              const suggestion = document.createElement('div');
-              suggestion.innerHTML = list[i];
-              
-              suggestion.addEventListener('click', function () {
-                  input.value = this.innerHTML;
-                  closeList();
-              });
-              suggestion.style.cursor = 'pointer';
-
-              suggestions.appendChild(suggestion);
-          }
-      }
-
-  });
-
-  function closeList() {
-      let suggestions = document.getElementById('suggestions');
-      if (suggestions)
-          suggestions.parentNode!.removeChild(suggestions);
+const input = document.getElementById('input')!;
+input.addEventListener('input', function () {
+  let data = JSON.parse(localStorage.getItem('myList')!);
+  const suggestions = document.querySelector('.suggestions ul')!;
+  suggestions.innerHTML = '';
+  if (data.length > 0) {
+    for (let i = 0; i < data.length; i++) {
+      console.log(data);
+      let item = data[i];
+      item = item.replace(item, `<strong>${item}</strong>`);
+      suggestions.innerHTML += `<li>${item}</li>`;
+    }
+    suggestions.classList.add('has-suggestions');
+  } else {
+    data = [];
+    suggestions.innerHTML = '';
+    suggestions.classList.remove('has-suggestions');
   }
+});
+function useSuggestion(e) {
+  const suggestions = document.querySelector('.suggestions ul')!;
+  const input = document.getElementById('input')!;
+  input.value = e.target.innerText;
+  input.focus();
+  suggestions.innerHTML = '';
+  suggestions.classList.remove('has-suggestions');
 }
-autocomplete(document.getElementById('input'), JSON.parse(localStorage.getItem('myList')!));
+const suggestions = document.querySelector('.suggestions ul')!;
+suggestions.addEventListener('click', useSuggestion);
