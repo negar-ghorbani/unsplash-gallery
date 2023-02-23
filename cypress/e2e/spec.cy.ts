a13a76a1b0461dc3dbaba21c5dee32870bfb6703
Negar Ghorbani <negarghorbani@Negars-Air.localdomain>
2023-02-23 13:37:31 +0100

    created readme

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,5 +1,5 @@
 describe('template spec', () => {
   it('passes', () => {
-    cy.visit('https://google.com')
+    cy.visit('https://negar-ghorbani.github.io/unsplash-gallery/')
   })
 })
--- src/main.spec.ts
+++ src/main.spec.ts
@@ -1,30 +1,7 @@
 import { describe, expect, test } from '@jest/globals';
-// import * as fetcher from './fetcher';
 
-// // const mockData = 'cats';
-// const url = `https://google.com/`;
-
-// describe('fetcher module', () => {
-//   test.only('should call fetchApi', async () => {
-//     jest.spyOn(fetcher.useSuggestion, 'fetchAPI')
-//     expect(fetcher.fetchAPI).toHaveBeenCalled()
-//   })
-//   test('should respond with 200', async () => {
-//     const returnedValue = await fetcher.fetchAPI(url)
-//     expect(returnedValue.status).toBe(200)
-//   })
-//   test('fetchUser should get data from the fetcher and return it', (done) => {
-//    fetcher.fetchAPI(url).then(data => {
-//      expect(data).toEqual({
-//        user: 'Ahsan'
-//       });
-//       done();
-//     })
-//   });
-// });
-
-describe('true be, or not true be', () => {
-  test('that is the question', async () => {
-    expect(true).toBe(true);
-  });
+describe('only testing if jest is working', () => {
+    test('true to be true', async () => {
+        expect(true).toBe(true);
+    });
 });
